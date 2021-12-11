6320a582f37806cc52708a7ef47aed861a7ead29
zafar <zafar142007@gmail.com>
2021-12-12 00:49:22 +0530

    increasing check delay

--- src/main/java/com/zafar/homeGenie/utils/Constants.java
+++ src/main/java/com/zafar/homeGenie/utils/Constants.java
@@ -6,13 +6,13 @@ public interface Constants {
     String ROUTER_TV_POWER = "routerTvPower";
     String RESULT = "RESULT";
     String TARGET_DEVICE = "samsung";
-    double RX_THRESHOLD_BYTES = 1000;
-    double TX_THRESHOLD_BYTES = 1000;
+    double RX_THRESHOLD_BYTES = 1200;
+    double TX_THRESHOLD_BYTES = 1200;
     String SHOULD_TV_BE_OFF = "SHOULD_TV_BE_OFF";
     String ABORT = "ABORT";
     String ABORT_REASON = "ABORT_REASON";
     String WEBCLIENT = "WEBCLIENT";
-    int NUMBER_OF_CONSECUTIVE_DETECTONS = 6;
+    int NUMBER_OF_CONSECUTIVE_DETECTONS = 7;
     String DATA_METRICS = "dataMetrics";
     int RETRY_OPERATION = 4;
 }
